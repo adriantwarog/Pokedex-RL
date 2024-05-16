2a6ca4e1cbe03038cf5379c7a93b217123fa316c
Adrian Twarog <adriantwarog@gmail.com>
2024-05-16 13:49:18 +0800

    updated endpoints

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -6,8 +6,8 @@ import OpenAI from "openai";
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 // Initialize the client
-const client = new DataAPIClient(process.env.ASTRA_TOKEN);
-const db = client.db('https://8ce1a9d9-fb59-4236-9d4c-451485cff59b-us-east-2.apps.astra.datastax.com');
+const client = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(process.env.ASTRA_DB_API_ENDPOINT);
 
 export async function GET(req) {
 
